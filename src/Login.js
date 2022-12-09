3aa0d7473934666e9a77f12501daa92b08799822
kukaa <kukaa904@gmail.com>
2022-12-09 17:09:22 +0530

    first commit

---
+++ src/Login.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import "./Login.css"
+
+function Login() {
+  return (
+    <>
+      <div class="login-box">
+
+
+<span></span>
+<span></span>
+<span></span>
+<span></span>
+<div className='logo'></div>
+
+<div class="inpt"><label>Email ID :</label></div>
+<div class="inpt"><input type="text" placeholder="Enter your email here..."></input></div>
+
+<div class="inpt"><label>Password :</label></div>
+<div class="inpt"><input type="password" placeholder="Enter your password..."></input></div>
+
+</div>
+
+    </>
+  )
+}
+
+export default Login
